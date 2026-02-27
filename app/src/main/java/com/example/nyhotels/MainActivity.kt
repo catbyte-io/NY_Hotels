073434580b669ce3a15f3dd7383a802e026c6c89
catbyte-io <catbyte.io@gmail.com>
2026-02-27 14:22:06 -0700

    Add Mapbox Maven repository

--- app/src/main/java/com/example/nyhotels/MainActivity.kt
+++ app/src/main/java/com/example/nyhotels/MainActivity.kt
@@ -3,45 +3,40 @@ package com.example.nyhotels
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.nyhotels.ui.theme.NYHotelsTheme
+import androidx.compose.ui.unit.dp
+import com.mapbox.geojson.Point
+import com.mapbox.maps.extension.compose.MapboxMap
+import com.mapbox.maps.extension.compose.animation.viewport.rememberMapViewportState
+import com.mapbox.maps.plugin.attribution.Attribution
+import com.mapbox.maps.plugin.scalebar.ScaleBar
 
-class MainActivity : ComponentActivity() {
+public class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            NYHotelsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            MapboxMap(
+                Modifier.fillMaxSize(),
+                mapViewportState = rememberMapViewportState {
+                    setCameraOptions {
+                        zoom(2.0)
+                        center(Point.fromLngLat(-98.0, 39.5))
+                        pitch(0.0)
+                        bearing(0.0)
+                    }
+                },
+                scaleBar = {
+                    ScaleBar(Modifier.padding(top = 60.dp))
+                },
+                logo = {
+                    Logo(Modifier.padding(bottom = 40.dp))
+                },
+                attribution = {
+                    Attribution(Modifier.padding(bottom = 40.dp))
                 }
-            }
+            )
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NYHotelsTheme {
-        Greeting("Android")
     }
 }
